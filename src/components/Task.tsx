ac1b8fd3e6eeb8442c19056254d96e506968a69b
MiKoding <mikaioharison038@gmail.com>
2024-02-24 15:10:12 -0300

    update: including task list camp

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,8 +1,21 @@
-
+import { Notebook } from "@phosphor-icons/react"
+import styles from "./Task.module.css"
 export function Task(){
     return (
+        <div>
+            <header className={styles.header}>
+                <p>Tarefas criadas</p>         
+                <p>Tarefas concluidas</p>
+            </header>
+            <div>
+                <Notebook size={16}/>
+                <p>você não tem tarefas cadastradas</p>
+                <p>crie tarefas e organize seus itens a fazer</p>
+            </div>
 
-    <div></div>
+        </div>
+
+        
         )
     
 }
